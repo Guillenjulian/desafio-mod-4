51ab0dd1031a31d29944f2c8e117dca458b266e9
Guillen Julian <guillen.julian1985@gmail.com>
2022-11-15 00:06:23 -0300

    desafio finalizado, se imnplementaron  ultimos retoque

--- page/servicios.js
+++ page/servicios.js
@@ -1,11 +1,11 @@
 function main() {
+  // esta funcion tre el formato del header
   headerCreater(document.querySelector(".headerContainer"));
 
-  getData().then(function (works) {
-    for (const w of works) {
-      addCards(w);
-    }
-  });
+  // esta funcion tre el formato de las cads
+  getData();
+  // esta funcion tre el formato del footer
+
   footerCreater(document.querySelector(".footer"));
 }
 main();
